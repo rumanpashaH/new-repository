e3776f894b21ead195ff27081aa1ca3f4b7a370c
rumanpashaH <rumanpasha98@gmail.com>
2019-10-21 11:08:50 +0530

    new commits

--- file3.py
+++ file3.py
@@ -3,5 +3,5 @@ def factorial(n):
 return 1 if (n==1 or n==0) else n * factorial(n - 1);  
   
 num = 5; 
-print("Factorial of",num,"is", 
+print("Factorial of the given number",num,"is", 
 factorial(num)) 
--- file4.py
+++ file4.py
@@ -5,6 +5,6 @@ T = 1
 SI = (P * R * T) / 100
   
  
-print("simple interest is", SI) 
+print("The simple interest is", SI) 
    
 
